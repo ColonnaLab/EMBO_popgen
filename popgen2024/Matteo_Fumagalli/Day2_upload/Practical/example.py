465aee4658bce52b7220cf97efb4b3d1741610f5
Matteo Fumagalli <mfumagalli@users.noreply.github.com>
2024-06-07 11:06:02 +0100

    add extra files for day2

---
+++ popgen2024/Matteo_Fumagalli/Day2_upload/Practical/example.py
@@ -0,0 +1,25 @@
+
+import msprime
+
+# Simulate 10 diploid samples under the coalescent with recombination on a 10kb region.
+ts = msprime.sim_ancestry(
+    samples=10,
+    recombination_rate=1e-8, # as in humans
+    sequence_length=10_000,
+    population_size=10_000,
+    random_seed=1234)
+
+# we can add mutations
+mutated_ts = msprime.sim_mutations(ts, rate=1e-8, random_seed=1234)
+print(mutated_ts.tables.sites)
+
+for variant in mutated_ts.variants():
+    print(variant)
+
+
+
+
+
+
+
+
