4bfe714fd41f981463fa829e653823da4847974c
steinrue <steinrue@uchicago.edu>
2025-06-23 00:36:55 +0200

    Some adjustments.

--- popgen2025/Matthias_Steinrücken/day2/practicals/example_simple_one.py
+++ popgen2025/Matthias_Steinrücken/day2/practicals/example_simple_one.py
@@ -2,11 +2,20 @@
 import msprime
 
 # Simulate 10 diploid samples under the coalescent with recombination on a 10kb region.
+
+# 100_000 individuals at present
+# 10_000 individuals before 1000 generations ago
+demography = msprime.Demography()
+demography.add_population(name="A", initial_size=100_000)
+# instantaneous reduction of size to 10_000
+demography.add_population_parameters_change(population="A", time=1000, initial_size=10_000)
+
+
 ts = msprime.sim_ancestry(
     samples=10,
     recombination_rate=1e-8, # as in humans
     sequence_length=10_000,
-    population_size=10_000,
+    demography=demography,
     random_seed=1234)
 
 # we can add mutations
